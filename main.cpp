e4df3493ea570c5342b57bfc017547038c648d46
abdelrahman hamdy <abdoe317111@gmail.com>
2023-01-12 02:20:22 +0200

    add node and gates  from file

--- main.cpp
+++ main.cpp
@@ -9,15 +9,44 @@ int main()
 {
     dynamicnode nodes; 
     gate*  a [50] ;
+     int n=-1;
     
-    
-    
-    
-    
+    fstream file1 ;
+    file1.open("data.txt",ios::in);
+            string x ;
+
+    while(!file1.eof()){
+        
+        file1>>x;
+        if (x=="and"){
+            n++;
+           a[n] = new andgate();
+           file1>>x;
+           while (!(x == "*")){
+               
+               nodes.add(x);
+               a[n]->addinput(nodes.find(x));
+               
+                           file1>>x;
+  
+           }
+           file1>>x;
+              nodes.add(x);
+               a[n]->setoutput(nodes.find(x));
+            
+        }
+        if (x=="set"){
+            
+            
+            
+        }
+        
+    }
     
      
     
-            
+                   nodes.printnodes();
+
     
                     
 
